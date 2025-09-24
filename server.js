e914af22ee732d7c7c031ce4cf1b1a6a1b3cb74d
t-miyazaki <t-miyazaki@CN24-R-0174.local>
2025-09-24 13:58:44 +0900

    crud 実装

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ fastify.register( fastifyMySQL,
   connectionString: `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
    })
 
-
+//read
 fastify.get('/user/:id', function(req, reply) {
   fastify.mysql.query(
     'SELECT id, name, age FROM user WHERE id=?', [req.params.id],
@@ -23,6 +23,65 @@ fastify.get('/user/:id', function(req, reply) {
     }
   )
 })
+
+//create
+fastify.post('/user', (req, reply) => {
+  const { name, age } = req.body;
+  
+  fastify.mysql.query(
+    'INSERT INTO user (name, age) VALUES (?, ?)',
+    [name, age],
+    (err, result) => {
+      if (err) {
+        reply.code(500).send(err);
+      } else {
+        reply.send({ id: result.insertId, name, age });
+      }
+    }
+  );
+});
+
+//update
+fastify.patch('/user/:id', (req, reply) => {
+  const { name, age } = req.body; // 更新する内容
+  const { id } = req.params;      // 更新対象のID
+
+  fastify.mysql.query(
+    'UPDATE user SET name = ?, age = ? WHERE id = ?',
+    [name, age, id],
+    (err, result) => {
+      if (err) {
+        reply.code(500).send(err);
+      } else if (result.affectedRows === 0) {
+        reply.code(404).send({ message: 'User not found' });
+      } else {
+        reply.send({ id, name, age });
+      }
+    }
+  );
+});
+
+//delete
+fastify.delete('/user/:id', (req,reply) => {
+  const { id } = req.params;
+
+  fastify.mysql.query(
+    'DELETE from user WHERE id = ?',
+    [id],
+    (err,result) => {
+      if (err){
+        reply.code(500).send(err);
+      }else{
+        reply.send({deletedId: id});
+      }
+    }
+  );
+})
+
+
+
+
+
 
 
 
