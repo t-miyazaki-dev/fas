a22a6c0c55d73a2cfd22c0bb017091e1da41b796
t-miyazaki <t-miyazaki@CN24-R-0174.local>
2025-09-24 18:18:41 +0900

    bag

---
+++ server.ts
@@ -0,0 +1,113 @@
+import Fastify from "fastify";
+
+import fastifyMySQL from "@fastify/mysql"
+
+import type { Pool, PoolConnection } from "mysql2";
+
+
+
+const fastify = Fastify({
+    logger:true
+})
+
+declare module "fastify" {
+  interface FastifyInstance {
+    mysql: {
+      query: Pool["query"];
+      getConnection: () => Promise<PoolConnection>;
+    };
+  }
+}
+
+
+fastify.register(fastifyMySQL, {
+  connectionString: `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+});
+
+//read
+fastify.get('/user/:id', function(req, reply) {
+  fastify.mysql.query(
+    'SELECT id, name, age FROM user WHERE id=?', [req.params.id],
+    function onResult (err, result) {
+      reply.send(err || result)
+    }
+  )
+})
+
+//create
+fastify.post('/user', (req, reply) => {
+  const { name, age } = req.body;
+  
+  fastify.mysql.query(
+    'INSERT INTO user (name, age) VALUES (?, ?)',
+    [name, age],
+    (err, result) => {
+      if (err) {
+        reply.code(500).send(err);
+      } else {
+        reply.send({ id: result.insertId, name, age });
+      }
+    }
+  );
+});
+
+//update
+fastify.patch('/user/:id', (req, reply) => {
+  const { name, age } = req.body; // 更新する内容
+  const { id } = req.params;      // 更新対象のID
+
+  fastify.mysql.query(
+    'UPDATE user SET name = ?, age = ? WHERE id = ?',
+    [name, age, id],
+    (err, result) => {
+      if (err) {
+        reply.code(500).send(err);
+      } else if (result.affectedRows === 0) {
+        reply.code(404).send({ message: 'User not found' });
+      } else {
+        reply.send({ id, name, age });
+      }
+    }
+  );
+});
+
+//delete
+fastify.delete('/user/:id', (req,reply) => {
+  const { id } = req.params;
+
+  fastify.mysql.query(
+    'DELETE from user WHERE id = ?',
+    [id],
+    (err,result) => {
+      if (err){
+        reply.code(500).send(err);
+      }else{
+        reply.send({deletedId: id});
+      }
+    }
+  );
+})
+
+
+
+
+
+
+
+
+
+fastify.get(`/health_check`,async(request,reply) => {
+    return `OK`;
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000,host: "0.0.0.0" });
+    console.log('サーバーがポート 3000 で起動しました。');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
